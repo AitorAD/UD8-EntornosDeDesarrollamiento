38045682ba37997f400cc85bdbe0bdcd5bbecd3f
INFORMATICA\dam_ada <aduqueagullo@gmail.com>
2024-05-14 13:04:45 +0200

    Creada la clase Hyperpar y la clase HyperparTest

---
+++ Tarea1UD8(Aitor)/src/algoritmes/Hyperpar.java
@@ -0,0 +1,49 @@
+package algoritmes;
+
+/**
+ * Clase Hyperpar
+ * @author Aitor Agullo Duque
+ * @version 1.0
+ */
+public class Hyperpar {
+    private long num;
+
+    /**
+     * Constructor por defecto
+     */
+    public Hyperpar(long num) {
+        this.num = num;
+    }
+
+    /**
+     * Getter numero
+     * @return
+     */
+    public long getNum() {
+        return num;
+    }
+
+    /**
+     * Setter numero
+     * @param num
+     */
+    public void setNum(long num) {
+        this.num = num;
+    }
+
+    /**
+     * Comprueba si el numero esta formado solamente por numeros pares.
+     * @return Devuelve verdadero en caso de que este formado solo por numeros pares. Devuelve falso si hay algun numero impar.
+     */
+    public boolean esHyperPar() {
+        char[] numerosSeparados = String.valueOf(this.num).toCharArray();
+
+        for (int i = 0; i < numerosSeparados.length; i++) {
+            int num = Integer.parseInt(String.valueOf(numerosSeparados[i]));
+            if (num % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Tarea1UD8(Aitor)/src/algoritmes/HyperparTest.java
@@ -0,0 +1,22 @@
+package algoritmes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HyperparTest {
+
+    @Test
+    public void tests_ElNumero_24680_es_HYPERPAR() {
+        Hyperpar hp = new Hyperpar(24680);
+        boolean esperado = true;
+        assertEquals(esperado, hp.esHyperPar());
+    }
+
+    @Test
+    public void tests_ElNumero_2568_noEs_HYPERPAR() {
+        Hyperpar hp = new Hyperpar(2568);
+        boolean esperado = false;
+        assertEquals(esperado, hp.esHyperPar());
+    }
+}
